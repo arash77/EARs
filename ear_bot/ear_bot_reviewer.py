dfae30933d774b444a42bda24eeef811eaa61f02
Arash <arash77.kad@gmail.com>
2024-05-14 19:19:56 +0200

    feat: Add functionality to find a reviewer for pull requests
    This commit modifies the `ear_bot_reviewer.py` script to add the functionality of finding a reviewer for pull requests. It introduces a new command-line argument `--search` which triggers the search for a reviewer if needed. The script now checks the existing pull requests and their associated comments to determine if a reviewer needs to be assigned. If a reviewer is required, a random reviewer is selected from the available list and a comment is posted on the pull request asking for their agreement. The script also updates the `artifact.json` file to keep track of the assigned reviewers and the date of the comment.

--- ear_bot/ear_bot_reviewer.py
+++ ear_bot/ear_bot_reviewer.py
@@ -1,6 +1,8 @@
 import json
 import os
 import random
+import sys
+from argparse import ArgumentParser
 from datetime import datetime, timedelta
 
 import pytz
@@ -17,60 +19,97 @@ list_of_reviewers = os.getenv("REVIEWERS", "").split(",")
 artifact_path = os.path.join("ear_bot", os.getenv("ARTIFACT_PATH", "artifact.json"))
 
 
-def comment_to_random_reviewer(pr, reviewers):
-    if not reviewers:
-        return []
-    reviewer = random.choice(reviewers)
-    current_date = datetime.now(tz=cet).replace(microsecond=0)
-    pr.create_issue_comment(
-        f"👋 Hi @{reviewer}, do you agree to review this assembly?\n\nPlease reply to this message only with Yes or No by {current_date + timedelta(days=7)}"
-    )
-    return [reviewer]
-
-
-def main():
+def find_reviewer(prs=[], deadline_check=True):
     save_pr_data = {}
     if os.path.exists(artifact_path):
         with open(artifact_path, "r") as file:
             save_pr_data = json.load(file)
+    if save_pr_data and not prs:
         for closed_pr in closed_pull_requests:
             closed_pr_number = str(closed_pr.number)
             if closed_pr_number in save_pr_data:
                 del save_pr_data[closed_pr_number]
 
     current_date = datetime.now(tz=cet).replace(microsecond=0)
-    for pr in pull_requests:
+
+    if not prs:
+        prs = pull_requests
+
+    for pr in prs:
+        if len(pr.requested_reviewers) > 1:
+            continue
         pr_number = str(pr.number)
-        if len(pr.requested_reviewers) < 2:
-            left_reviewers = list_of_reviewers
-            old_reviewers = save_pr_data.get(pr_number, {}).get(
-                "requested_reviewers", []
-            )
-            comment_date = save_pr_data.get(pr_number, {}).get("date")
+        pr_data = save_pr_data.get(pr_number, {})
+        old_reviewers = pr_data.get("requested_reviewers", [])
+        if set(old_reviewers) == set(list_of_reviewers):
+            old_reviewers = []  # Reset the reviewers if all reviewers have been asked
+        left_reviewers = list(set(list_of_reviewers) - set(old_reviewers))
+        date = pr_data.get("date", current_date)
+        if deadline_check:
             for comment in pr.get_issue_comments().reversed:
                 text_to_check = "Please reply to this message only with Yes or No by"
                 if comment.user.type == "Bot" and text_to_check in comment.body:
-                    comment_date = comment.created_at.astimezone(cet)
-                    deadline = comment_date + timedelta(days=7)
-                    comment_date = comment_date.isoformat()
-                    left_reviewers = (
-                        list(set(list_of_reviewers) - set(old_reviewers))
-                        if deadline < current_date
-                        else []
-                    )
+                    old_reviewers = comment.body.split("Hi @")[1].split(",")[0].split()
+                    date = comment.created_at.astimezone(cet)
+                    deadline = date + timedelta(days=7)
+                    if deadline > current_date:
+                        left_reviewers = []
                     break
-            new_reviewer = comment_to_random_reviewer(pr, left_reviewers)
-            save_pr_data[pr_number] = {
-                "date": (
-                    current_date.isoformat()
-                    if new_reviewer or not comment_date
-                    else comment_date
-                ),
-                "requested_reviewers": old_reviewers + new_reviewer,
-            }
+
+        new_reviewer = []
+        if left_reviewers:
+            reviewer = random.choice(left_reviewers)
+            pr.create_issue_comment(
+                f"👋 Hi @{reviewer}, do you agree to review this assembly?\n\n"
+                + "Please reply to this message only with Yes or No by"
+                + f" {current_date + timedelta(days=7)}"
+            )
+            new_reviewer = [reviewer]
+
+        save_pr_data[pr_number] = {
+            "date": date,
+            "requested_reviewers": old_reviewers + new_reviewer,
+        }
     with open(artifact_path, "w") as file:
-        json.dump(save_pr_data, file, indent=4)
+        json.dump(save_pr_data, file, indent=4, default=str)
+
+
+def assign_reviewer():
+    comment_text = os.getenv("COMMENT_TEXT")
+    comment_author = os.getenv("COMMENT_AUTHOR")
+    pr_number = os.getenv("PR_NUMBER")
+    if not comment_text or not comment_author or not pr_number:
+        print("Missing required environment variables.")
+        sys.exit(1)
+    pr = repo.get_pull(int(pr_number))
+    if comment_author in [rr.login for rr in pr.requested_reviewers]:
+        print("The reviewer has already been assigned.")
+        sys.exit()
+    if "yes" in comment_text.lower():
+        pr.create_review_request([comment_author])
+    elif "no" in comment_text.lower():
+        find_reviewer([pr], deadline_check=False)
+    else:
+        print("Invalid comment text.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(description="EAR bot!")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--search", action="store_true", help="Search for a reviewer if needed."
+    )
+    group.add_argument(
+        "--comment",
+        action="store_true",
+        help="Assign the reviewer to the PR when the reviewer agrees.",
+    )
+    args = parser.parse_args()
+    if args.search:
+        find_reviewer()
+    elif args.comment:
+        assign_reviewer()
+    else:
+        parser.print_help()
+        sys.exit(1)
