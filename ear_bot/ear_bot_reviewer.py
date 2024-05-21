461a0cd411b29aebaaa25116035692fe3cf850a7
Arash <arash77.kad@gmail.com>
2024-05-21 14:40:28 +0200

    chore: Update GitHub workflows for ear_bot
    - Added busy reviewers option
    - Removed unnecessary condition in the 'comment-reviewer' job
    - Added condition to the 'request-reviewer' job to only run for pull requests with the 'ERGA-BGE' label
    - Moved the 'SUPERVISOR_PATH' environment variable to the 'approved-changes' job
    - Updated the condition in the 'approved-changes' job to only run when the review state is 'approved' and the pull request has the 'ERGA-BGE' label
    - Updated the 'Create comment for supervisor and reviewer' step in the 'approved-changes' job to remove the condition based on the review state

--- ear_bot/ear_bot_reviewer.py
+++ ear_bot/ear_bot_reviewer.py
@@ -19,17 +19,27 @@ artifact_path = os.path.join("ear_bot", os.getenv("ARTIFACT_PATH", "artifact.jso
 reviewers_path = os.path.join("ear_bot", os.getenv("REVIEWERS_PATH", "reviewers.txt"))
 if os.path.exists(reviewers_path):
     with open(reviewers_path, "r") as file:
-        list_of_reviewers = file.read().splitlines()
+        list_of_reviewers = set(map(str.lower, file.read().splitlines()))
 else:
     print("Missing reviewers file.")
     sys.exit(1)
 
 
-def find_reviewer(prs=[], deadline_check=True):
+def load_pr_data():
     save_pr_data = {"pr": {}, "busy_reviewers": []}
     if os.path.exists(artifact_path):
         with open(artifact_path, "r") as file:
             save_pr_data = json.load(file)
+    return save_pr_data
+
+
+def dump_pr_data(save_pr_data):
+    with open(artifact_path, "w") as file:
+        json.dump(save_pr_data, file, indent=4, default=str)
+
+
+def find_reviewer(prs=[], deadline_check=True):
+    save_pr_data = load_pr_data()
     if save_pr_data and not prs:
         for closed_pr in closed_pull_requests:
             closed_pr_number = str(closed_pr.number)
@@ -42,72 +52,72 @@ def find_reviewer(prs=[], deadline_check=True):
         prs = pull_requests
 
     for pr in prs:
-        if len(pr.requested_reviewers) > 1:
+        if len(pr.requested_reviewers) > 1 or "ERGA-BGE" not in [
+            label.name for label in pr.get_labels()
+        ]:
             continue
         pr_number = str(pr.number)
         pr_data = save_pr_data["pr"].get(pr_number, {})
-        old_reviewers = pr_data.get("requested_reviewers", [])
-        if set(map(str.lower, old_reviewers)) == set(map(str.lower, list_of_reviewers)):
-            old_reviewers = []  # Reset the reviewers if all reviewers have been asked
-        left_reviewers = list(set(list_of_reviewers) - set(old_reviewers))
+        old_reviewers = set(map(str.lower, pr_data.get("requested_reviewers", [])))
+        busy_reviewers = set(map(str.lower, save_pr_data.get("busy_reviewers", [])))
+
+        if old_reviewers | busy_reviewers == list_of_reviewers:
+            old_reviewers = set()
+            busy_reviewers = set()
+        left_reviewers = list_of_reviewers - old_reviewers - busy_reviewers
         date = pr_data.get("date", current_date)
         if deadline_check:
             for comment in pr.get_issue_comments().reversed:
                 text_to_check = "Please reply to this message only with Yes or No by"
                 if comment.user.type == "Bot" and text_to_check in comment.body:
-                    comment_reviewer = comment.body.split("Hi @")[1].split(",")[0]
-                    (
-                        old_reviewers.append(comment_reviewer)
-                        if comment_reviewer not in old_reviewers
-                        else None
+                    comment_reviewer = (
+                        comment.body.split("Hi @")[1].split(",")[0].lower()
                     )
+                    old_reviewers.update(comment_reviewer)
                     date = comment.created_at.astimezone(cet)
                     deadline = date + timedelta(days=7)
                     if deadline > current_date:
-                        left_reviewers = []
+                        left_reviewers = set()
                     break
 
-        new_reviewer = []
+        new_reviewer = set()
         if left_reviewers:
-            reviewer = random.choice(left_reviewers)
+            reviewer = random.choice(list(left_reviewers))
             pr.create_issue_comment(
                 f"👋 Hi @{reviewer}, do you agree to review this assembly?\n\n"
                 "Please reply to this message only with Yes or No by"
                 f" **{current_date + timedelta(days=7)}**"
             )
-            new_reviewer = [reviewer]
+            new_reviewer = {reviewer}
 
         save_pr_data["pr"][pr_number] = {
             "date": date,
-            "requested_reviewers": old_reviewers + new_reviewer,
+            "requested_reviewers": list(old_reviewers | new_reviewer),
         }
 
-    with open(artifact_path, "w") as file:
-        json.dump(save_pr_data, file, indent=4, default=str)
+    dump_pr_data(save_pr_data)
 
 
 def assign_reviewer():
-    comment_text = os.getenv("COMMENT_TEXT")
-    comment_author = os.getenv("COMMENT_AUTHOR")
+    comment_text = os.getenv("COMMENT_TEXT").lower()
+    comment_author = os.getenv("COMMENT_AUTHOR").lower()
     pr_number = os.getenv("PR_NUMBER")
     if not comment_text or not comment_author or not pr_number:
         print("Missing required environment variables.")
         sys.exit(1)
     pr = repo.get_pull(int(pr_number))
-    if comment_author in [rr.login for rr in pr.requested_reviewers]:
+    if comment_author in [rr.login.lower() for rr in pr.requested_reviewers]:
         print("The reviewer has already been assigned.")
         sys.exit()
     for comment in pr.get_issue_comments().reversed:
         text_to_check = "Please reply to this message only with Yes or No"
         if comment.user.type == "Bot" and text_to_check in comment.body:
-            comment_reviewer = comment.body.split("Hi @")[1].split(",")[0]
+            comment_reviewer = comment.body.split("Hi @")[1].split(",")[0].lower()
             break
-    print(f"Comment author: {comment_author}")
-    print(f"Comment reviewer: {comment_reviewer}")
-    if comment_author.lower() != comment_reviewer.lower():
+    if comment_author != comment_reviewer:
         print("The reviewer is not the one who was asked to review the PR.")
         sys.exit(1)
-    if "yes" in comment_text.lower():
+    if "yes" in comment_text:
         supervisor_path = os.path.join(
             "ear_bot", os.getenv("SUPERVISOR_PATH", "supervisor.txt")
         )
@@ -123,10 +133,10 @@ def assign_reviewer():
             "Please check the Wiki if you need to refresh something.\n"
             f"Contact the @{supervisor} for any issues."
         )
-        # busy_reviewers = save_pr_data["busy_reviewers"]
-        # busy_reviewers.append(comment_author)
-        # save_pr_data["busy_reviewers"] = busy_reviewers
-    elif "no" in comment_text.lower():
+        save_pr_data = load_pr_data()
+        save_pr_data["busy_reviewers"].append(comment_author)
+        dump_pr_data(save_pr_data)
+    elif "no" in comment_text:
         find_reviewer([pr], deadline_check=False)
     else:
         print("Invalid comment text.")
